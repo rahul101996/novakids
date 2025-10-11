25c6acf2e1abe7874fd420916a3bd539fc66368a
Rahul Sachwani <119300258+rahul101996@users.noreply.github.com>
2025-10-11 15:48:05 +0530

    policy pages added

---
+++ views/website/tearms-and-condition.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/views/website/include/header.php'; ?>
+
+<body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/views/website/include/navbar.php'; ?>
+
+
+    <div class="w-full  bg-white flex justify-center items-start my-16 max-md:my-8">
+        <div class="w-[60%] max-md:w-[90%] flex items-center justify-center flex-col">
+
+            <h1 class="text-5xl max-md:text-3xl font-semibold">Terms & Condition</h1>
+            <div class="text-gray-600 mt-6">
+                <p>
+                    For the purpose of these Terms and Conditions, the terms "we", "us", "our" used anywhere on this page shall mean <strong>NOVAKIDS</strong>, whose registered/operational office is Unit no. 135, Globe Business Park, Ambarnath Badlapur Road, Ambarnath West, Dist. Thane, Maharashtra 421501. The terms "you", “your”, "user", or “visitor” shall mean any natural or legal person who is visiting our website and/or has agreed to purchase from us. Your use of the website and/or purchase from us is governed by the following Terms and Conditions:
+
+                    The content of the pages of this website is subject to change without notice. Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness, or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors, and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.
+
+                    Your use of any information or materials on our website and/or product pages is entirely at your own risk, for which we shall not be liable. It is your responsibility to ensure that any products, services, or information available through our website and/or product pages meet your specific requirements.
+
+                    Our website contains material which is owned by or licensed to us. This material includes, but is not limited to, the design, layout, look, appearance, and graphics. Reproduction is prohibited other than in accordance with the copyright notice, which forms part of these Terms and Conditions. All trademarks reproduced on our website which are not the property of, or licensed to, the operator are acknowledged on the website. Unauthorized use of information provided by us shall give rise to a claim for damages and/or may be considered a criminal offense.
+
+                    From time to time, our website may also include links to other websites. These links are provided for your convenience to provide further information. You may not create a link to our website from another website or document without <strong>NOVAKIDS</strong>’s prior written consent.
+
+                    Any dispute arising out of the use of our website and/or purchase with us and/or any engagement with us is subject to the laws of India. We shall be under no liability whatsoever in respect of any loss or damage arising directly or indirectly out of the decline of authorization for any transaction, on account of the cardholder having exceeded the preset limit mutually agreed by us with our acquiring bank from time to time.
+                </p>
+
+            </div>
+
+        </div>
+    </div>
+
+
+
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/views/website/include/footer.php'; ?>
+</body>
+
+</html>
